3a30d0a5afb4793395cdc38fb388016ab313f627
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-09-07 12:17:05 -0500

    [+] Components: Navigation, CreateNote, CreateList, NotesList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Navigation from './components/Navigation';
+import NotesList from './components/NotesList';
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navigation/>
         <h1>Notes App</h1>
         <h3>MERN Stack (API RestFul)</h3>
       </header>
