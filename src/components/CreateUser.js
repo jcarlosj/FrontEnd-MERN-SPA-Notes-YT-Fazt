20b2ceda0c711311ce5d6bb94762d2e485384ccc
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-09-07 13:56:43 -0500

    [+] HTTP: Instala 'axios', Hace petición al API y obtiene datos
    Aclaración:
       El Siguiente mensaje en la terminal y en la consola se produce
       por dejar espacios entre los objetos y los metodos al llamarlos
    
       objeto .metodo()		Se produce el mensaje
       objeto.metodo()		Se evita el mensaje escribiendo asi
    
    Mensaje ADVERTENCIA (terminal/consola)
    
    Compiled with warnings.
    
    ./src/components/CreateUser.js
      Line 8:  Unexpected whitespace before property get  no-whitespace-before-property
      Line 9:   Unexpected whitespace before property log  no-whitespace-before-property
    
    Search for the keywords to learn more about each warning.
    To ignore, add // eslint-disable-next-line to the line before

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,6 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';                  // Paquete que permite hacer peticiones HTTP
 
 export default class CreateUser extends Component {
+    // Método: Ejecuta acciones una vez el componente a sido montado
+    async componentDidMount() {
+        /** Realiza petición HTTP al API usando axios */
+        const users = await axios .get( 'http://localhost:4000/api/users' );    // Es una operación Asincrona por lo que se puede usar: un callback, una Promesa o en este último caso convertir nuestra funcion en una funcion asincrona. Podria usarse el tipico fetch() de JavaScript Promise, etc
+        console .log( users );
+    }
+
     render() {
         return (
             <div>
