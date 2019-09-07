1d8bd3d4546b0df18eda6fe8f5cd69eea2867066
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-09-07 16:35:20 -0500

    [+] React: Define el estado de los datos obtenidos en el componente

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import axios from 'axios';                  // Paquete que permite hacer peticiones HTTP
 
 export default class CreateUser extends Component {
+
+    // Define el estado de los datos en el Componente
+    state = {
+        users: []
+    }
+
     // Método: Ejecuta acciones una vez el componente a sido montado
     async componentDidMount() {
         /** Realiza petición HTTP al API usando axios */
         const users = await axios .get( 'http://localhost:4000/api/users' );    // Es una operación Asincrona por lo que se puede usar: un callback, una Promesa o en este último caso convertir nuestra funcion en una funcion asincrona. Podria usarse el tipico fetch() de JavaScript Promise, etc
         console .log( users );
+
+        // Almacena los datos en el Estado de la Aplicación
+        this .setState({
+            users: users .data
+        });
+        console .log( 'Estado de usuarios', this .state .users );
     }
 
     render() {
